5e65a5628bc9288784f24ee2d16b0b32ed31ce25
jerryfan <vanms@sina.com>
2025-03-14 22:27:32 +0800

    add pom and main class

---
+++ src/main/java/com/kangda/HellpSpringboot.java
@@ -0,0 +1,12 @@
+package com.kangda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellpSpringboot
+{
+    public static void main( String[] args )    {
+        SpringApplication.run(HellpSpringboot.class, args);
+    }
+}
