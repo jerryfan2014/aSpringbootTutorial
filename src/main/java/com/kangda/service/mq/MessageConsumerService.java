eb3e9d2cfa62961dcbd216766ad4eb558fe60064
jerryfan <jerryfan@kdvtc.edu.cn>
2025-06-04 11:37:56 +0800

    mq消息队列

--- src/main/java/com/kangda/service/mq/MessageConsumerService.java
+++ src/main/java/com/kangda/service/mq/MessageConsumerService.java
@@ -1,24 +1,24 @@
-package com.kangda.service.mq;
-
-
-import com.kangda.po.User;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageConsumerService {
-
-    @RabbitListener(queues = "email.queue")
-    public void psubConsumEmail(Message message) {
-        byte[] bytes = message.getBody();
-        System.out.println("邮件队列接收到消息：" + new String(bytes));
-
-    }
-
-    @RabbitListener(queues = "sms.queue")
-    public void psubConsumEmail(User user) {
-        System.out.println("短信队列接收到消息：" + user.toString());
-    }
-
-}
+//package com.kangda.service.mq;
+//
+//
+//import com.kangda.po.User;
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MessageConsumerService {
+//
+//    @RabbitListener(queues = "email.queue")
+//    public void psubConsumEmail(Message message) {
+//        byte[] bytes = message.getBody();
+//        System.out.println("邮件队列接收到消息：" + new String(bytes));
+//
+//    }
+//
+//    @RabbitListener(queues = "sms.queue")
+//    public void psubConsumEmail(User user) {
+//        System.out.println("短信队列接收到消息：" + user.toString());
+//    }
+//
+//}
--- src/main/java/com/kangda/service/mq/MessageProducerService.java
+++ src/main/java/com/kangda/service/mq/MessageProducerService.java
@@ -1,43 +1,43 @@
-package com.kangda.service.mq;
-
-import com.kangda.po.User;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageProducerService {
-    private AmqpAdmin amqpAdmin;
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public MessageProducerService(AmqpAdmin amqpAdmin,
-                                  RabbitTemplate rabbitTemplate){
-        this.amqpAdmin = amqpAdmin;
-        this.rabbitTemplate = rabbitTemplate;
-
-        //定义exchange 为广播模式 fanout
-        this.amqpAdmin.declareExchange(
-                new FanoutExchange("pub/sub.exchange"));
-        //创建queue
-        this.amqpAdmin.declareQueue(new Queue("email.queue"));
-        this.amqpAdmin.declareQueue(new Queue("sms.queue"));
-        //bingding
-        this.amqpAdmin.declareBinding(new Binding("email.queue",
-                Binding.DestinationType.QUEUE,
-                "pub/sub.exchange","", null));
-        this.amqpAdmin.declareBinding(new Binding("sms.queue",
-                Binding.DestinationType.QUEUE,
-                "pub/sub.exchange","", null));
-    }
-
-    public void psubPubulish(User user) {
-        this.rabbitTemplate.convertAndSend("pub/sub.exchange",
-                "", user);
-    }
-
-}
+//package com.kangda.service.mq;
+//
+//import com.kangda.po.User;
+//import org.springframework.amqp.core.AmqpAdmin;
+//import org.springframework.amqp.core.Binding;
+//import org.springframework.amqp.core.FanoutExchange;
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MessageProducerService {
+//    private AmqpAdmin amqpAdmin;
+//    private RabbitTemplate rabbitTemplate;
+//
+//    @Autowired
+//    public MessageProducerService(AmqpAdmin amqpAdmin,
+//                                  RabbitTemplate rabbitTemplate){
+//        this.amqpAdmin = amqpAdmin;
+//        this.rabbitTemplate = rabbitTemplate;
+//
+//        //定义exchange 为广播模式 fanout
+//        this.amqpAdmin.declareExchange(
+//                new FanoutExchange("pub/sub.exchange"));
+//        //创建queue
+//        this.amqpAdmin.declareQueue(new Queue("email.queue"));
+//        this.amqpAdmin.declareQueue(new Queue("sms.queue"));
+//        //bingding
+//        this.amqpAdmin.declareBinding(new Binding("email.queue",
+//                Binding.DestinationType.QUEUE,
+//                "pub/sub.exchange","", null));
+//        this.amqpAdmin.declareBinding(new Binding("sms.queue",
+//                Binding.DestinationType.QUEUE,
+//                "pub/sub.exchange","", null));
+//    }
+//
+//    public void psubPubulish(User user) {
+//        this.rabbitTemplate.convertAndSend("pub/sub.exchange",
+//                "", user);
+//    }
+//
+//}
